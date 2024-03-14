fd6c204c65393d872eb99c09210226c592b8f50f
ant0-nn <anton.250605@gmail.com>
2024-03-15 01:26:43 +0200

    upgrate

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'http://localhost:5000/api',
+    baseURL: 'http://139.28.37.125:8000/api',
 });
 
 export default instance;
